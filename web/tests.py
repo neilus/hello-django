45fe2cbd0c92ae844ee65170ff821b83d29bd897
Norbert Sana <neilus@elte.hu>
2020-03-14 23:25:30 +0100

    A hello world test

--- web/tests.py
+++ web/tests.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.utils import timezone
+from django.test import Client
+from django.http import HttpResponse
 
-# Create your tests here.
+from .models import Question
+
+class GetHelloWorldTests(TestCase):
+
+    def test_hello(self):
+        client = Client()
+        response = client.get('/')
+
+        self.assertContains(response, "Hello", status_code=200)
+
